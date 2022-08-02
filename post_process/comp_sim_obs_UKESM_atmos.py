44314c58f6359b4eff48064e0671e6e24794103b
SimonTett <simon.tett@ed.ac.uk>
2022-08-02 16:23:43 +0100

    Moved land frac file to config, modified config json file to reflect that change, and some minor changes to post processing.

--- post_process/comp_sim_obs_UKESM_atmos.py
+++ post_process/comp_sim_obs_UKESM_atmos.py
@@ -18,8 +18,8 @@ import numpy as np
 import xarray
 
 import StudyConfig
-
-
+lookup_std=dict() # where we hold std names lookup
+lookup_long = dict() # where we hold long names lookup
 
 def guess_lat_lon_vert_names(dataArray):
     """
@@ -32,6 +32,7 @@ def guess_lat_lon_vert_names(dataArray):
     """
     if dataArray is None:
         return None, None, None
+
     def find_name(dimensions, patterns):
         name = None
         for pattern in patterns:
@@ -116,6 +117,9 @@ def names(dataset, name=None):
     return lookup
 
 
+
+
+
 def genProcess(dataset, land_mask, latitude_coord=None):
     """
     Setup the processing information
@@ -126,6 +130,8 @@ def genProcess(dataset, land_mask, latitude_coord=None):
     :return: dict containing data to be processed.
     """
     # create long & standard name lookup
+    global  lookup_std
+    global lookup_long
     lookup_std = names(dataset)
 
     lookup_long = names(dataset, name='long')
@@ -143,36 +149,21 @@ def genProcess(dataset, land_mask, latitude_coord=None):
     # delta_p = model_delta_p(dataset.surface_air_pressure,
     #                         dataset.UM_atmosphere_hybrid_sigma_pressure_coordinate_ak_bounds,
     #                         dataset.UM_atmosphere_hybrid_sigma_pressure_coordinate_bk_bounds)
-    delta_p = None # not bothering to set up delta P as not actually used.
-
-    def extract_ds(dataset,name,coord=None,mask=None):
-
-        var = lookup_std.get(name,lookup_long.get(name)) # try standard name first. If not found use long name
-        if var is None:
-            print(f"Failed to find {name} in standard or long list ")
-            return None # no name found so return empty
-
-        da = dataset[var]
-
-        if mask is not None:
-            da=da.where(mask,np.nan)
-        if coord is not None:
-            da=da.sel(coord,method='nearest')
-        return da
+    delta_p = None  # not bothering to set up delta P as not actually used.
 
     process = {
-        #'TEMP@50': dataset[lookup_long['TEMPERATURE ON P LEV/T GRID']].sel(coord_50hPa, method='nearest'),
-        'TEMP@500': extract_ds(dataset,'TEMPERATURE ON P LEV/T GRID',coord=coord_500hPa),
-        'RH@500': extract_ds(dataset,'RELATIVE HUMIDITY ON P LEV/T GRID',coord=coord_500hPa),
-        'OLR': extract_ds(dataset,'toa_outgoing_longwave_flux'),
-        'OLRC': extract_ds(dataset,'toa_outgoing_longwave_flux_assuming_clear_sky'),
-        'RSR': extract_ds(dataset,'toa_outgoing_shortwave_flux'),
-        'RSRC': extract_ds(dataset,'toa_outgoing_shortwave_flux_assuming_clear_sky'),
-        'INSW': extract_ds(dataset,'toa_incoming_shortwave_flux'),
-        'LAT': extract_ds(dataset,'TEMPERATURE AT 1.5M',coord={latitude_coord: constrain_60S},mask=land_mask),
+        # 'TEMP@50': dataset[lookup_long['TEMPERATURE ON P LEV/T GRID']].sel(coord_50hPa, method='nearest'),
+        'TEMP@500': extract_ds(dataset, 'TEMPERATURE ON P LEV/T GRID', coord=coord_500hPa),
+        'RH@500': extract_ds(dataset, 'RELATIVE HUMIDITY ON P LEV/T GRID', coord=coord_500hPa),
+        'OLR': extract_ds(dataset, 'toa_outgoing_longwave_flux'),
+        'OLRC': extract_ds(dataset, 'toa_outgoing_longwave_flux_assuming_clear_sky'),
+        'RSR': extract_ds(dataset, 'toa_outgoing_shortwave_flux'),
+        'RSRC': extract_ds(dataset, 'toa_outgoing_shortwave_flux_assuming_clear_sky'),
+        'INSW': extract_ds(dataset, 'toa_incoming_shortwave_flux'),
+        'LAT': extract_ds(dataset, 'TEMPERATURE AT 1.5M', coord={latitude_coord: constrain_60S}, mask=land_mask),
         'Lprecip': extract_ds(dataset, 'precipitation_flux', coord={latitude_coord: constrain_60S}, mask=land_mask),
         'MSLP': extract_ds(dataset, 'air_pressure_at_sea_level'),
-        #'REFF': dataset['UM_m01s01i245'] / dataset['UM_m01s01i246'],
+        # 'REFF': dataset['UM_m01s01i245'] / dataset['UM_m01s01i246'],
         # HadXM3 carries all S compounds as mass of S. So SO2 & DMS need conversion to SO2 & DMS for
         # comparison with obs. Code below will need updating for HadGEM-GA10?
         # 'SO2_col': total_column(dataset.mass_fraction_of_sulfur_dioxide_in_air, delta_p) * \
@@ -182,8 +173,8 @@ def genProcess(dataset, land_mask, latitude_coord=None):
         # 'DMS_col': total_column(dataset.mass_fraction_of_dimethyl_sulfide_in_air, delta_p) * \
         #            simonLib.mole_wt['DMS'] / simonLib.mole_wt['S'],
         # 'O3_col_DU': total_column(dataset['UM_m01s02i260'], delta_p) * simonLib.DU['O3'],
-        #'Trop_SW_up': dataset.tropopause_upwelling_shortwave_flux,
-        #'Trop_SW_net': dataset.tropopause_net_downward_shortwave_flux
+        # 'Trop_SW_up': dataset.tropopause_upwelling_shortwave_flux,
+        # 'Trop_SW_net': dataset.tropopause_net_downward_shortwave_flux
     }
 
     process['netflux'] = process['INSW'] - process['RSR'] - process['OLR']
@@ -191,6 +182,19 @@ def genProcess(dataset, land_mask, latitude_coord=None):
     return process
 
 
+def extract_ds(dataset, name, coord=None, mask=None):
+    var = lookup_std.get(name, lookup_long.get(name))  # try standard name first. If not found use long name
+    if var is None:
+        print(f"Failed to find {name} in standard or long list ")
+        return None  # no name found so return empty
+
+    da = dataset[var]
+
+    if mask is not None:
+        da = da.where(mask, np.nan)
+    if coord is not None:
+        da = da.sel(coord, method='nearest')
+    return da
 def means(dataArray, name, latitude_coord=None):
     """ 
     Compute means for NH extra tropics, Tropics and SH extra Tropics. 
@@ -269,18 +273,17 @@ def do_work():
     config = StudyConfig.readConfig(args.CONFIG)
     options = config.getv('postProcess', {})
 
-
     # work out the files if needed
     if args.dir is None:
         rootdir = pathlib.Path.cwd()
     else:
         rootdir = pathlib.Path(os.path.expandvars(args.dir))
     files = list(rootdir.glob('*.nc'))
-      
+
     mask_file = options['mask_file']
     mask_file = pathlib.Path(os.path.expandvars(mask_file)).expanduser()
     mask_name = options['mask_name']
-    mask_fraction = options.get('mask_fraction') # if not present will be None
+    mask_fraction = options.get('mask_fraction')  # if not present will be None
     start_time = options['start_time']
     end_time = options['end_time']
     if args.output is None:
@@ -296,30 +299,29 @@ def do_work():
         print("dir", rootdir)
         print("mask_file", mask_file)
         print("land_mask", mask_name)
-        print("mask_fraction",mask_fraction)
+        print("mask_fraction", mask_fraction)
         print("start_time", start_time)
         print("end_file", end_time)
         print("output", output_file)
         if verbose > 1:
             print("options are ", options)
     # UM ancillary times are usually  year 0 which cases grief if attempt to decode
-    land_mask = xarray.load_dataset(mask_file,decode_times=False)[mask_name].squeeze()  # land/sea mask
+    land_mask = xarray.load_dataset(mask_file, decode_times=False)[mask_name].squeeze()  # land/sea mask
     if mask_fraction is not None:
         land_mask = land_mask >= mask_fraction
     latitude_coord = options.get('latitude_coord', None)
-    # code below does not work when data is on my M drive on my laptop...
-    dataset = xarray.open_mfdataset(files)#.sortby('time')  # sortby is really important as want co-ords to be monotonic
+    dataset = xarray.open_mfdataset(files)
     # need to rename various elements
-    rename =dict(TMONMN='time',TMONMN_rad='time_rad',
-                 PLEV10='air_pressure',
-                 latitude_t='latitude',longitude_t='longitude')
+    rename = dict(TMONMN='time', TMONMN_rad='time_rad',
+                  PLEV10='air_pressure',
+                  latitude_t='latitude', longitude_t='longitude')
     dataset = dataset.rename(rename)
     # now change time_rad to time.
-    #check time_rad values = time values
+    # check time_rad values = time values
     if (dataset.time == dataset.time_rad).all():
-        dataset = dataset.reset_index(['time_rad'],drop=True)
+        dataset = dataset.reset_index(['time_rad'], drop=True)
         dataset = dataset.rename(dict(time_rad='time'))
-    dataset = dataset.sortby('time') # sort by time coords.
+    dataset = dataset.sortby('time')  # sort by time coords.
     dataset = dataset.sel(time=slice(start_time, end_time))
 
     process = genProcess(dataset, land_mask, latitude_coord=latitude_coord)
@@ -339,9 +341,9 @@ def do_work():
         summary[k + '_DGM'] = summary.pop(k) - summary['MSLP_GLOBAL']
 
     # lower case all names
-    results=dict()
-    for k,v in summary.items():
-        results[k.lower()]=v
+    results = dict()
+    for k, v in summary.items():
+        results[k.lower()] = v
     if verbose:  # print out the summary data for all created values
         for name, value in results.items():
             if value is None:
@@ -355,6 +357,8 @@ def do_work():
         json.dump(results, fp, indent=2)
 
     return results
+
+
 # TODO add  some test cases...
 
 import unittest
