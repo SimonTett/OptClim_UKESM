5dbd8b98cbea76d1d8f306e71c2314b7e8477ad8
SimonTett <simon.tett@ed.ac.uk>
2025-09-18 14:41:59 +0100

    requirmenets.txt now compatable with python 3.10; bug fix to simobs_wrapper and sorted out pp_config.json

--- post_process/simobs_wrapper.sh
+++ post_process/simobs_wrapper.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #SBATCH --qos=serial
 #SBATCH --partition=serial
-#SBATCH --account=nn02-TERRAFIRMA
-#SBATCH --time=00:30:00
+#SBATCH --account=n02-TERRAFIRMA
+#SBATCH --time=00:10:00 # processing ~ 17 months takes circa 2 mins
 #SBATCH --mem=16G
 #SBATCH --job-name=my_simobs
 #SBATCH --output=my_simobs_%j.out
 #SBATCH --error=my_simobs_%j.err
-#SBATH --user=tetts
 #SBATCH --export=ALL
-#export OPT_UKESM_ROOT=/work/n02/shared/tetts/OptClim_UKESM # check this value
-source "${OPT_UKESM_ROOT}"/setup_archer2 # sets up the environment
-echo calling: comp_sim_obs_UKESM_atmos.py  "$@"
-comp_sim_obs_UKESM_atmos.py  "$@" # then run the command
+# Sadly need to set up the env on ARCHER2.
+# WHich is why this script is needed.
+source /work/n02/shared/tetts/OptClim_UKESM/setup_archer2 # sets up the environment
+cmd="${OPT_UKESM_ROOT}/post_process/comp_sim_obs_UKESM1_1.py  $@"
+echo calling: $cmd
+$cmd # run the command
+
 
